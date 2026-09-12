4b51f80fd58c7c8b1fee84342cfedf14d848db58
furqan1448 <maram1998m3@gmail.com>
2026-09-13 00:05:52 +0300

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -4,7 +4,7 @@
 
 // ⚠️ حطي هنا رابط الـ Web app اللي طلعلك من Google Apps Script بعد الـ Deploy
 // (لازم ينتهي بـ /exec)
-const API_URL = "https://script.google.com/macros/s/AKfycbzdRDEyGV-aVqNdV8FYqj46Rk-nexb_fSWkcn6CMU2UVX56l4gnX1TqVc8NbK2YTNIS/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyb6pIBVqa-jr27Ni5mTRCcMPIae7ZuObRpq8Meol75t2nubinLpTwWWD_EXOyVP2wc/exec";
 
 export async function callApi(action, data) {
   if (API_URL.indexOf("PASTE_YOUR") === 0) {
